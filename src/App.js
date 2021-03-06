db98fd3ba2c72d05ec48717bbec3acadf5cf2694
finesaaa <indriatifiqey16@gmail.com>
2021-03-06 15:04:50 +0700

    fix hook etc

--- src/App.js
+++ src/App.js
@@ -1,106 +1,71 @@
-import React, { useState, Component } from 'react';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Card from '@material-ui/core/Card';
+import ShisenAppBar from './components/ShisenAppBar';
+import ShisenSnackbar from './components/ShisenSnackbar';
 import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import TextField from '@material-ui/core/TextField';
 import ROSLIB from 'roslib';
 
-const ros = new ROSLIB.Ros();
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: 'ws://localhost:9090',
-      connected: false,
-    };
+function App() {
+  const classes = useStyles();
+  const [url, setURL] = useState('ws://localhost:9090');
+  const [connected, setConnected] = useState(false);
+  const ros = new ROSLIB.Ros();
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleConnect = this.handleConnect.bind(this);
-  }
-
-  handleChange(event) {
-    event.preventDefault();
-    this.setState({value: event.target.value});
-  }
-
-  handleConnect(event) {
-    event.preventDefault();
-    ros.connect(this.state.value);
-
-    ros.on('connection', () => {
-      this.setConnected(true);
-    });
+  ros.on('connection', () => {
+    setConnected(true);
+    showMessage("Connected", "success");
+  });
   
-    ros.on('error', () => {
-      this.setConnected(false);
-      alert('error');
-    });
+  ros.on('error', () => {
+    setConnected(false);
+    showMessage("Error", "error");
+  });
   
-    ros.on('close', () => {
-      this.setConnected(false);
-      alert('close');
-    });
-  }
+  ros.on('close', () => {
+    setConnected(false);
+    showMessage("Close", "warning");
+  });
 
-  setConnected(bool) {
-    this.setState({connected: bool})
-  }
+  const handleConnect = () => {
+    ros.connect(url);
+  };
 
-  render () {
-    if (this.state.connected) {
-      // main interface
-      return (
-        <div>
-          <AppBar position="static" color="primary">
-            <Toolbar>
-              <Typography variant="h6">
-                Shisen App
-              </Typography>
-            </Toolbar>
-          </AppBar>
-          <Card>
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
-                ICHIRO-ITS - Connected {this.state.value}
-              </Typography>
-            </CardContent>
-          </Card>
-        </div>
-      );
-    } else {
-      // connection interface
-      return (
-        <div>
-          <AppBar position="static" color="primary">
-            <Toolbar>
-              <Typography variant="h6">
-                Shisen App
-              </Typography>
-            </Toolbar>
-          </AppBar>
-          <Card>
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
-                ICHIRO-ITS - Disconnect
-              </Typography>
-              <TextField label="URL WebSocket" variant="filled" value={this.state.value} onChange={(e) => this.handleChange(e)} />
-            </CardContent>
-            <CardActions>
-              <Button variant="contained" color="primary" onClick={(e) => this.handleConnect(e)}>
-                Connect
-              </Button>
-            </CardActions>
-          </Card>
-        </div>
-      );
-    }
-  }
+  const onChangeHandler = event => {
+    setURL(event.target.value);
+  };
+
+  return (
+    <div className={classes.root}>
+      <ShisenAppBar></ShisenAppBar>
+      <Card>
+        <CardContent>
+          { (!connected)  ? <TextField label="URL WebSocket" variant="filled" value={url} onChange={onChangeHandler} /> : ''}
+        </CardContent>
+        <CardActions>
+          { (!connected) ? <Button variant="outlined" onClick={handleConnect}>Connect</Button> : ''} 
+          <div id="snackbar"/>
+        </CardActions>
+      </Card>
+    </div>
+  );
 }
 
 export default App;
+
+function showMessage(message, type) {
+  ReactDOM.render(<ShisenSnackbar type={type} message={message}/>, document.getElementById('snackbar'));
+}
