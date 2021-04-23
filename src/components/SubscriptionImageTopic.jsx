650732cdc19db713093c0c248c61d100ddbe9909
finesaaa <indriatifiqey16@gmail.com>
2021-04-23 10:16:50 +0700

    add components

---
+++ src/components/SubscriptionImageTopic.jsx
@@ -0,0 +1,108 @@
+import {
+  Box,
+  Grid,
+  List,
+  ListItem,
+  ListItemText,
+  TextField,
+  Typography,
+} from "@material-ui/core";
+
+import {
+  BoxedCircularProgress,
+  TitledCard,
+  useNode,
+  useSubscription,
+} from "kumo-app";
+
+import React, { useState } from "react";
+import { v4 as uuid } from "uuid";
+
+function SubscriptionImageTopic() {
+  const node = useNode("img_topic_subscription");
+
+  const [messages, setMessages] = useState([]);
+
+  const [topicName, setTopicName] = useState("camera/image_raw");
+
+  const subscription = useSubscription(
+    node,
+    "std_msgs/msg/String",
+    topicName,
+    (message) => {
+      setMessages((prevMessages) => {
+        const newMessages = prevMessages.slice();
+        newMessages.unshift({
+          id: uuid(),
+          data: message.data,
+        });
+
+        return newMessages;
+      });
+    }
+  );
+
+  const Content = () => {
+    if (subscription === null) {
+      return <BoxedCircularProgress />;
+    }
+
+    const MessageList = () => {
+      if (messages.length <= 0) {
+        return (
+          <Box
+            display="flex"
+            height="100%"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Typography>No Data</Typography>
+          </Box>
+        );
+      }
+
+      const messageItems = messages.map((message) => {
+        return (
+          <ListItem key={message.id} button divider>
+            <ListItemText primary={message.data} />
+          </ListItem>
+        );
+      });
+
+      return <List disablePadding>{messageItems}</List>;
+    };
+
+    return (
+      <Box height={200} overflow="auto">
+        <MessageList />
+      </Box>
+    );
+  };
+
+  const onTopicNameChange = (event) => {
+    setTopicName(event.target.value);
+  };
+
+  return (
+    <TitledCard title="Subscription Image Topic" raised disablePadding>
+      <Box m={2} pt={2}>
+        <Grid spacing={2}>
+          <Grid item xs={12}>
+            <TextField
+              label="Topic Name"
+              value={topicName}
+              onChange={onTopicNameChange}
+              disabled={subscription}
+              variant="outlined"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Content />
+          </Grid>
+        </Grid>
+      </Box>
+    </TitledCard>
+  );
+}
+
+export default SubscriptionImageTopic;
