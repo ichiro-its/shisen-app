2f196943c37793552679538160e8c3d3ec758d3d
Alffaza <ridzkiraihanalfaza@gmail.com>
2021-08-26 16:45:27 +0700

    feat: add subscriber in capture settings component

--- src/components/CaptureSettings.jsx
+++ src/components/CaptureSettings.jsx
@@ -1,7 +1,15 @@
-import { Box, Grid, TextField } from "@material-ui/core";
-import { TitledCard } from "kumo-app";
+import { Box, Grid, TextField, Typography } from "@material-ui/core";
+
+import {
+  BoxedCircularProgress,
+  TitledCard,
+  useNode,
+  useSubscription,
+} from "kumo-app";
 
 import React, { useState } from "react";
+
+import { v4 as uuid } from "uuid";
 
 function CaptureSettings() {
   const [config, setConfig] = useState({
@@ -13,6 +21,38 @@ function CaptureSettings() {
     gain: 0,
   });
 
+  const node = useNode("img_topic_subscription");
+
+  const [messages, setMessages] = useState([]);
+
+  const [topicName, setTopicName] = useState("camera/capture_setting_event");
+
+  const subscription = useSubscription(
+    node,
+    "shisen_interfaces/msg/CaptureSetting",
+    topicName,
+    (message) => {
+      setMessages((prevMessages) => {
+        const newMessages = prevMessages.slice();
+        newMessages.unshift({
+          id: uuid(),
+          brightness: message.brightness,
+          contrast: message.contrast,
+          saturation: message.saturation,
+          temperature: message.temperature,
+          hue: message.hue,
+          gain: message.gain,
+        });
+
+        return newMessages;
+      });
+    }
+  );
+
+  const onTopicNameChange = (event) => {
+    setTopicName(event.target.value);
+  };
+
   const handleChange = (event) => {
     setConfig({
       ...config,
@@ -20,9 +60,25 @@ function CaptureSettings() {
     });
   };
 
-  return (
-    <TitledCard title="Camera Settings" overflow="auto">
-      <Box>
+  const Content = () => {
+    if (subscription == null) {
+      return <BoxedCircularProgress />;
+    }
+
+    const ConfigSettings = () => {
+      if (messages.length <= 0) {
+        return (
+          <Box
+            display="flex"
+            height="100%"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Typography>No Data</Typography>
+          </Box>
+        );
+      }
+      return (
         <Grid container direction="row" spacing={1}>
           <Grid item xs={4}>
             <TextField
@@ -83,6 +139,33 @@ function CaptureSettings() {
               variant="outlined"
               onChange={handleChange}
             />
+          </Grid>
+        </Grid>
+      );
+    };
+    return (
+      <Box height={200} overflow="auto">
+        <ConfigSettings />
+      </Box>
+    );
+  };
+
+  return (
+    <TitledCard title="Camera Settings" overflow="auto" raised disablePadding>
+      <Box m={2} pt={2}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField
+              label="Topic Name"
+              value={topicName}
+              variant="outlined"
+              onChange={onTopicNameChange}
+              disabled={subscription}
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Content />
           </Grid>
         </Grid>
       </Box>
